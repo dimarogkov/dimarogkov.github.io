d68ab570941a6e35497adaff23b4507c03fb5111
dima <ror_developer_5@itsites.com.ua>
2020-05-07 14:58:16 +0300

    new

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
-	// Пункты портфолио
-	$('.nav__link').click(function(event) {
-		if ($('.nav__link').hasClass('active')) {
-			$('.nav__link').not($(this)).removeClass('active');
-		}
+	$('.menu__link').click(function(event) {
+		$('.menu__link').not($(this)).removeClass('active');
 		$(this).toggleClass('active');
 	})
+
+	$('a[href^="#"]').click(function(){ 
+		let anchor = $(this).attr('href');  
+			$('html, body').animate({           
+			scrollTop:  $(anchor).offset().top  
+		}, 600);                           
+	});
+
 	// Фиксированное меню
 	$('.header').removeClass('active');
     $(window).scroll(function(){
@@ -15,58 +20,25 @@ $(document).ready(function() {
             $('.header').removeClass('active').fadeIn('fast');
         };
     })
-    // Открывающейся меню
-    $('.header__menu').click(function(event) {
-		$('.header__menu, .menu').toggleClass('active');
-		$('body').toggleClass('lock');
-		$('.footer__des').not($(this).next()).slideUp(300);
-	})
 
-	// Service price
-	$('.price__block').click(function(event) {
-		if ($('.price__block').hasClass('active')) {
-			$('.price__block').not($(this)).removeClass('active');
-		}
-		$(this).toggleClass('active');
-	})
-	// Blog
-	$('.post__like').click(function(event) {
-		if ($('.post__like').hasClass('active')) {
-			$('.post__like').not($(this)).removeClass('active');
-		}
-		$(this).toggleClass('active')
-	})
-	$('.post__comment').click(function(event) {
-		if ($('.post__comment').hasClass('active')) {
-			$('.post__comment').not($(this)).removeClass('active');
-		}
-		$(this).toggleClass('active');
-	})
-	$('.pos__text').click(function(event) {
-		if ($('.pos__text').hasClass('active')) {
-			$('.pos__text').not($(this)).removeClass('active');
-		}
-		$(this).toggleClass('active');
-	})
-	$('.tags__text').click(function(event) {
-		if ($('.tags__text').hasClass('active')) {
-			$('.tags__text').not($(this)).removeClass('active');
-		}
-		$(this).toggleClass('active');
-	})
+    // Slider 
+	$('.slider').slick({
+		dots:true,
+		slidesToShow:1,
+		slidesToScroll:1,
+		autoplay:true,
+		autoplaySpeed:2000,
+	});
 
-
-	// Media Blog
-
-	$('.names').click(function(event) {
-		if ($('.names').hasClass('open')) {
-			$('.names').not($(this)).removeClass('active');
-			$('.sub__block').not($(this).next()).slideToggle('slow');
-		}
-		$(this).toggleClass('active').next('.sub__block').slideToggle(300);
-	})
-})
-
-		
-		
+	$(function(){
+		$('.scroll_top').click(function(){
+			$('html, body').animate({scrollTop: 0}, 600);
+			return false;
+		});
+	});
 	
+});
+
+
+
+
