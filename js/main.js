06026b250217178a1ded4814be766dfea26c74a4
dimarogkov <webdevs1.dima.rogkov@gmail.com>
2020-12-28 07:10:26 +0200

    ol.html ol.css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-	 
+	// anchor
+	 $(window).scroll(function(){
+        if ($(this).scrollTop()>0) {
+            $('.anchor').addClass('active');
+        } else {
+            $('.anchor').removeClass('active');
+        };
+    });
+ 	$('.anchor').on('click', function(event) {
+		event.preventDefault();
+
+		let block_top = $('.header__inner').offset().top;
+		$('html, body').animate({
+			scrollTop: -block_top
+		}, 900)
+	});
+
+
 });
