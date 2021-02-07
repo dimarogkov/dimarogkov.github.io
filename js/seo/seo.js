f542a9f63d2a467ab4f5dbc154be9019fd542425
dimarogkov <webdevs1.dima.rogkov@gmail.com>
2021-02-07 17:50:28 +0200

    seo update

--- js/seo/seo.js
+++ js/seo/seo.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 
 	$('.more-text .read-more').on('click', function(){
-		$(this).parents('.more-text').toggleClass('open-more-text');
+		let seoBtn = $('.more-text .read-more'); 
+		let more = seoBtn.attr('data-read-more');
+		let less = seoBtn.attr('data-read-less');
+
+		$(this).parents('.more-text').toggleClass('open');
 		$(this).parent().find('.text').slideToggle(300);
+
+	    $('.more-text').hasClass('open') ? $(this).text(less) : $(this).text(more);
 	});
 
 })
