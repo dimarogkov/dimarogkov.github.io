d1a1a17beff4f90385ac7f036eed932bc9828d34
dimarogkov <webdevs1.dima.rogkov@gmail.com>
2021-06-12 22:03:21 +0300

    upload.html upload.css upload.js

--- js/calendar/daterangepicker-init.js
+++ js/calendar/daterangepicker-init.js
@@ -10,12 +10,8 @@ jQuery(function($) {
             var $this = $(this);
             var dateFormatString = 'DD.MM.YYYY';
             var invalidDates = [
-                // '21.03.2020',
-                // '25.03.2020',
-                // '06.03.2020'
             ];
             var minDate = $this.data('no-min-date') ? 0 : new Date(),
-                // showDropdowns = $this.data('show-dropdowns') ? true : false,
                 isInvalideDates = $this.data('no-invalid-dates') ? false : function(date){
                     for(var i = 0; i < invalidDates.length; i++){
                         if (date.format(dateFormatString) == invalidDates[i]) {
@@ -28,11 +24,7 @@ jQuery(function($) {
                 "singleDatePicker": true,
                 autoUpdateInput: false,
                 minDate: minDate,
-                // showDropdowns: showDropdowns,
                 "isInvalidDate" : isInvalideDates,
-                // isInvalidDate: function(date) {
-                //     return (date.day() == 0 || date.day() == 6);
-                // },
                 "locale": {
                     "format": dateFormatString,
                     "separator": " - ",
@@ -71,7 +63,6 @@ jQuery(function($) {
 
             $this.on('apply.daterangepicker', function(ev, picker) {
                 $(this).val(picker.startDate.format(dateFormatString));
-                // $(this).closest('.input-wrapper').addClass('dirty');
                 $(this).closest('.toggle-block').find('select').html('');
             });
 
@@ -84,7 +75,6 @@ jQuery(function($) {
             });
 
             $this.on('hide.daterangepicker', function() {
-                // $(this).closest('.input-wrapper').removeClass('active');
                 $('.daterangepicker').removeClass('active');
             });
         });
---
+++ js/upload/upload.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	$(document).on('change', '.upload input', function (e){
+    	let img = URL.createObjectURL(e.target.files[0]);
+	    $(this).closest('.upload').find('.upload-imgs img').attr('src', img);
+  	});
+
+});
