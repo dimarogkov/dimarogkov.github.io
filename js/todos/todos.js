dd3690ba3ae233775c3f4b0f5b5d2d1d03862335
dimarogkov <webdevs1.dima.rogkov@gmail.com>
2022-01-22 19:57:49 +0200

    fix todos

--- js/todos/todos.js
+++ js/todos/todos.js
@@ -3,22 +3,12 @@ let _functions = {};
 $(document).ready(function() {
 
 	_functions.addTodo = () => {
-		let todo = $('.todos-item:first').clone();
-
-		todo.removeClass('disabled');
-		todo.find('.task-input').val('');
-		todo.find('.time-input').val('');
-		todo.find('.selected ').removeClass('red yellow blue').addClass('grey').attr('data-name-priority', 'Clear');
-		todo.find('.todo-time-posted').attr('data-post-status', false).find('input').prop('checked', false);
-		$('.todos-body').append(todo);
+		let item = '<div class="todos-item"><div class="todos-col"><input class="task-input" type="text" name="task" placeholder="Write Task"></div><div class="todos-col"><div class="todo-priority-block"><span class="selected grey" data-name-priority="Clear"><i class="far fa-flag"></i></span><ul class="todo-priority-list"><li data-class="red" class="red"><i class="far fa-flag"></i> Urgent</li><li data-class="yellow" class="yellow"><i class="far fa-flag"></i> Hight</li><li data-class="blue" class="blue"><i class="far fa-flag"></i> Normal</li><li data-class="grey" class="grey"><i class="far fa-flag"></i> Clear</li></ul></div></div><div class="todos-col"><input class="time-input time-tracked" type="number" min="0" step="1" name="timeTracked" placeholder="hour"></div><div class="todos-col"><input class="time-input time-estimate" type="number" min="0" step="1" name="timeEstimate" placeholder="hour"></div><div class="todos-col"><div class="todo-time-posted" data-post-status="false"><input type="checkbox" name="timePost"><span></span></div><div class="todo-remove"><i class="fas fa-times"></i></div></div></div>';
+		$('.todos-body').append(item);
 	}
 
 	_functions.removeTodo = (el) => {
 		el.parent().parent().remove();
-	}
-
-	_functions.checkTodosItems = () => {
-		$('.todos-item').length < 2 ? $('.todo-remove').addClass('hide') : $('.todo-remove').removeClass('hide');
 	}
 
 	_functions.openLayer = () => {
@@ -52,7 +42,7 @@ $(document).ready(function() {
 	}
 
 	_functions.setData = (item) => {
-		let todos = [];
+		let tasks = [];
 
 		$('.todos-item').each(function() {
 			let th = $(this);
@@ -65,16 +55,16 @@ $(document).ready(function() {
 					time_post: th.find('.todo-time-posted').attr('data-post-status')
 				};
 
-			todos.push(task);
+			tasks.push(task);
 		});
 
-		localStorage.setItem('todos', JSON.stringify(todos));
+		localStorage.setItem('tasks', JSON.stringify(tasks));
 	}
 
 	_functions.getData = () => {
-		if (localStorage.getItem('todos') !== null) {
+		if (localStorage.getItem('tasks') !== null) {
 
-			let data = JSON.parse(localStorage.getItem('todos'));
+			let data = JSON.parse(localStorage.getItem('tasks'));
 			
 			$('.todos-item:first').remove();
 
@@ -108,10 +98,7 @@ $(document).ready(function() {
 					timePostStatus = '';
 				}
 
-				console.log(priority);
-				console.log(priorityClass);
-
-				localTodo = '<div class="todos-item ' + disabledStatus + '"><div class="todos-col"><input class="task-input" type="text" name="task" placeholder="Write Task" value="' + task + '"></div><div class="todos-col"><div class="todo-priority-block"><span class="selected ' + priorityClass + '" data-name-priority="' + priority + '"><i class="far fa-flag"></i></span><ul class="todo-priority-list"><li data-class="red" class="red"><i class="far fa-flag"></i> Urgent</li><li data-class="yellow" class="yellow"><i class="far fa-flag"></i> Hight</li><li data-class="blue" class="blue"><i class="far fa-flag"></i> Normal</li><li data-class="grey" class="grey"><i class="far fa-flag"></i> Clear</li></ul></div></div><div class="todos-col"><input class="time-input time-tracked" type="number" min="0" step="1" name="timeTracked" placeholder="hour" value="' + timeTracked + '"></div><div class="todos-col"><input class="time-input time-estimate" type="number" min="0" step="1" name="timeEstimate" placeholder="hour" value="' + timeEstimate + '"></div><div class="todos-col"><div class="todo-time-posted" data-post-status="' + timePost +'"><input type="checkbox" name="timePost" '+ timePostStatus +'><span></span></div><div class="todo-remove"><i class="fas fa-times"></i></div></div></div>'
+				localTodo = '<div class="todos-item ' + disabledStatus + '"><div class="todos-col"><input class="task-input" type="text" name="task" placeholder="Write Task" value="' + task + '"></div><div class="todos-col"><div class="todo-priority-block"><span class="selected ' + priorityClass + '" data-name-priority="' + priority + '"><i class="far fa-flag"></i></span><ul class="todo-priority-list"><li data-class="red" class="red"><i class="far fa-flag"></i> Urgent</li><li data-class="yellow" class="yellow"><i class="far fa-flag"></i> Hight</li><li data-class="blue" class="blue"><i class="far fa-flag"></i> Normal</li><li data-class="grey" class="grey"><i class="far fa-flag"></i> Clear</li></ul></div></div><div class="todos-col"><input class="time-input time-tracked" type="number" min="0" step="1" name="timeTracked" placeholder="hour" value="' + timeTracked + '"></div><div class="todos-col"><input class="time-input time-estimate" type="number" min="0" step="1" name="timeEstimate" placeholder="hour" value="' + timeEstimate + '"></div><div class="todos-col"><div class="todo-time-posted" data-post-status="' + timePost +'"><input type="checkbox" name="timePost" '+ timePostStatus +'><span></span></div><div class="todo-remove"><i class="fas fa-times"></i></div></div></div>';
 				$('.todos-body').append(localTodo);
 			});
 		}
@@ -122,7 +109,6 @@ $(document).ready(function() {
 		let th = $(this);
 
 		_functions.addTodo();
-		_functions.checkTodosItems();
 		_functions.setData(th.closest('.todos-item'));
 	});
 
@@ -131,7 +117,6 @@ $(document).ready(function() {
 		let th = $(this);
 
 		_functions.removeTodo(th);
-		_functions.checkTodosItems();
 		_functions.setData(th.closest('.todos-item'));
 	});
 
@@ -166,13 +151,12 @@ $(document).ready(function() {
 		_functions.setData(th.closest('.todos-item'));
 	});
 	
-	$(document).on('change', '.task-input, .time-tracked, .time-estimate', function(e) {
+	$(document).on('keyup', '.task-input, .time-tracked, .time-estimate', function(e) {
 		e.preventDefault();
 		let th = $(this);
 
 		_functions.setData(th.closest('.todos-item'));
 	});
 
-	_functions.checkTodosItems();
 	_functions.getData();
 });
